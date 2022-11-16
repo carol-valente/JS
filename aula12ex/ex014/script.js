44911c73ec8b3fd27542771f98c90f4904359e95
Carol-Valente <108620693+Carol-Valente@users.noreply.github.com>
2022-11-16 18:43:15 -0300

    exercicios 24 e 25 concluidos

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,16 +3,19 @@ function carregar() {
   var img = window.document.getElementById('imagem');
   var data = new Date();
   //var hora = data.getHours();
-  var hora = 20;
-  msg.innerHTML = `Agora são ${hora} horas`;
+  var hora = 15;
+  msg.innerHTML = `Agora são ${hora} horas. <br>`;
   if (hora >= 0 && hora < 12) {
     img.src = 'imagens/manha-transp.png';
+    msg.innerHTML += `Bom dia!`;
     document.body.style.background = '#af99a0';
   } else if (hora >= 12 && hora < 18) {
     img.src = 'imagens/tarde-transp.png';
+    msg.innerHTML += `Boa tarde! Que tal um cafezinho? <3`;
     document.body.style.background = '#b26420';
   } else {
     img.src = 'imagens/noite-transp.png';
+    msg.innerHTML += `Tenha uma excelente noite!`;
     document.body.style.background = '#262c2c';
   }
 }

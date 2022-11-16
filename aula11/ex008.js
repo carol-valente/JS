f07e3555846cadc14ad154f6aa7af24182ac7764
Carol-Valente <108620693+Carol-Valente@users.noreply.github.com>
2022-11-16 12:08:02 -0300

    meus exercicios de JS

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 100;
+console.log(`A velocidade do seu carro é de ${vel} km/h.`);
+if (vel > 60) {
+  console.log('você ultrapassou a velocidade permitida. MULTADO!!!');
+} else {
+  console.log('Parabéns!!! Você está dirigindo de forma segura!!');
+}
+console.log('Dirija sempre utilizando cinto de segurança');
